27579d907dff74aed336abd35f54eceaadf7c6a3
Sam <35293833+sarbajeetmadnal4@users.noreply.github.com>
2021-09-10 14:33:02 +1000

    sarbajeet update

---
+++ sarbajeet update/index.php
@@ -0,0 +1,275 @@
+<?php
+require "header.php";
+?>
+
+<header class="header">
+    <div class="row">
+        <div class="col-md-12 text-center"><a class="logo"><a class="logo"><img src="img/logo1.png" alt="logo" width="599" height="236"></a></a></div>
+        <div class="col-md-12 text-center">
+            <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-light btn-lg"><em>Make a Reservation Now!</em></button>
+        </div>
+    </div>
+</header>
+
+
+
+<!--about us section-->
+
+<section id="aboutus">
+
+ <div class="container">
+   <h3 class="text-center"><br><br>Savoy Grill-Bar and Restaurant </h3>
+   <div class="row">
+<!--carousel-->
+     <div class="col-sm"><br><br>
+      	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+         <ol class="carousel-indicators">
+           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+         </ol>
+        <div class="carousel-inner">
+           <div class="carousel-item active">
+             <img class="d-block w-100" src="img/3.png" alt="First slide">
+           </div>
+           <div class="carousel-item">
+           <img class="d-block w-100" src="img/4.jpeg" alt="Second slide">
+           </div>
+           <div class="carousel-item">
+           <img class="d-block w-100" src="img/5.jpeg" alt="Third slide">
+           </div>
+        </div>
+         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+         </a>
+       </div><br><br>
+     </div>
+
+<!--end of carousel-->
+
+     <div class="col-sm">
+    	<div class="arranging"><br><hr>
+	<h4 class="text-center">Our Story</h4>
+	<p><br>Famiglia Romano<br>
+
+        <!-- Design the website for “About Us” navigational option and locate it on the top of the homepage next to the Menu containing"Ashok" -->
+        
+I was born in Henley Beach.
+My schooling was at the Star of the Sea Primary & St Michael's College. My boys and daughter are second generation attending the same schools as myself.
+Over the last 30 years I've seen Henley beach grow from a small suburb to a vibrant and thriving precinct. I believe Henley Beach has become an iconic go to destination!
+My passion as a farmer and love of our country has led me to open a local fresh grill and bar restaurant realising a dream of mine. I really believe it's something we need in our community.
+The Leopardi house and restaurant are yours to enjoy as well. We look forward to having the pleasure of serving you all.
+John Romano and family.
+Ciao.
+<br><br><br></p><hr>
+	</div>
+     </div>
+    </div><br>
+  </div>
+</section>
+<!--end of about us section-->
+
+
+<!-- Ensure all the food items have the correct short description in regards to the food ingredients, how it is made and what it is served with.
+"saurav" -->
+<!--Menu section-->
+<!-- Design the website for “Menu” navigational option and locate it on the top of the homepage
+      "Ashok" -->
+
+<section id="Menu">
+
+ <div class="container">
+   <h3 class="text-center"><br><br>Menu </h3>
+   <div class="row">
+
+
+     <div class="col-sm">
+        <div class="arranging"><br><hr>
+    <h4 class="text-center">List of food and drinks</h4>
+    <p><br>Enjoy<br>
+<h2>RED</h2>
+
+<!-- Insert the contents of the menu inside menu option 
+           "Dipendra" -->
+
+
+           <!-- Insert the short description for every food items of the menu list "Alisha" -->
+<h1><b>HUSTLE & VINE SANGIOVESE </b> $48</h1> McLaren Vale, South Australia                     
+
+
+<h1><b>HEWITSON ‘MISS HARRY’ GSM </b>                      $52</h1>
+Barossa Valley, South Australia
+<h1><b>ASHTON HILLS PINOT NOIR   </b>                  $64</h1>
+ Piccadilly Valley, South Australia
+
+<h1><b>MORDRELLE MALBEC</b>                            $64</h1>
+Langhorne Creek, South Australia
+<h1><b>BARRY BROS SHIRAZ CABERNET </b>                     $48</h1>
+Clare Valley, South Australia
+<h1><b>SC PANNELL TEMPRANILLO TOURIGA </b>                   $52</h1>
+McLaren Vale, South Australia
+<h1><b>GRANT BURGE ‘HOLY TRINITY’ GSM</b>                    $89</h1>
+Barossa Valley, South Australia
+    <br> </br>
+
+TAP BEER & CIDER
+<h1><b>FURPHY</b>                                   $ 9.0</h1>
+<h1><b>HAHN SUPERDRY </b>                            $9.0</h1>
+<h1><b>SUPERDRY 3.5 </b>                                 $7.5</h1>
+<h1><b>HEINEKEN</b>                                  $9.5</h1>
+<h1><b>JAMES SQUIRE PALE ALE </b>                        $9.5</h1>
+<h1><b>JAMES SQUIRE APPLE CIDER </b>                         $9.5</h1>
+<h1><b>COOPERS PALE ALE </b>                             $8.5</h1>
+<h1><b>MOON DOG ‘OLD MATE’ PALE ALE </b>                     $10</h1>
+<h1><b>PRANCING PONY PALE ALE  </b>                      $10</h1>
+<h1><b>LORD NELSON ‘3 SHEETS’ PALE ALE </b>                  $10</h1>
+
+<br><br><br></p><hr>
+    </div>
+     </div>
+    </div><br>
+  </div>
+</section>
+<!--end of Menu section-->
+
+<div class="header2">
+</div>
+
+<!----gallery -->
+<div class id="gallery"><br>
+    <div class="container">
+    <h3 class="text-center"><br>Gallery<br><br></h3>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+           <div class="d-flex flex-column">
+              <img src="img/1.png" class="img-fluid">
+              <img src="img/2.png" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+              <img src="img/3.png" class="img-fluid">
+              <img src="img/4.jpeg" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+               <img src="img/5.jpeg" class="img-fluid">
+               <img src="img/6.jpeg" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+               <img src="img/7.jpeg" class="img-fluid">
+               <img src="img/8.jpeg" class="img-fluid">
+           </div>
+        </div>
+    </div>
+</div><br><br>
+<!----end of gallery -->
+
+<div class="container" id="reservation">
+    <h3 class="text-center"><br><br>Reservation<br><br></h3>
+    <img  src="img/16.jpg" class="img-fluid rounded">
+    <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-dark btn-block btn-lg">Make a reservation Now!</button>
+        
+</div><br><br>
+
+<div class="header2">
+</div>
+<!-- Customize the contact us page with restaurant phone number, restaurant email address and postal address.  -->
+<!-- main page map section-->
+<section class="map" id="footer">
+    <div class="container">
+    <h3 class="text-center"><br><br>Find us!</h3><br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d3150.1829966583578!2d23.75232003459512!3d37.85600838634192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m3!3m2!1d37.8566691!2d23.752137599999998!4m0!5e0!3m2!1sel!2sgr!4v1524459240043" style= "width:100%;  height:250px; border:0;" allowfullscreen></iframe>
+    
+        <div class="row staff">
+            <div class="col">
+            <h4><strong>Opening Hours</strong></h4>
+                       
+                <div class="signup-form">
+                    <form action="#footer" method="post">
+                        <div class="form-group">
+                            <label>Enter Date</label>
+                            <input type="date" class="form-control" name="date" placeholder="Date" required="required">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="check_schedule" class="btn btn-dark btn-block">Check Open Time</button>
+                        </div>
+                    </form>
+                    
+<?php
+
+if(isset($_POST['check_schedule'])){
+      
+require 'includes/dbh.inc.php';
+            
+$date= $_POST['date'];
+ 
+    $sql = "SELECT * FROM schedule WHERE date = '$date'";
+    $result = $conn->query($sql);
+    if ($result->num_rows == 1) {
+        while($row = $result->fetch_assoc()) {
+            echo"
+                <table class='table table-sm table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Date</th>
+                    <th scope='col'>Open Time</th>
+                    <th scope='col'>Close Time</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>".$row['open_time']."</td>
+                    <td>".$row['close_time']."</td>
+                    </tr>
+                   </tbody>
+                </table>";
+                }
+            }
+        else{
+         echo"
+                <table class='table table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Date</th>
+                    <th scope='col'>Open Time</th>
+                    <th scope='col'>Close Time</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>12:00</td>
+                    <td>00:00</td>
+                    </tr>
+                   </tbody>
+                </table>";
+            }
+         
+   //close connection
+   mysqli_close($conn);
+}
+?>
+                        
+                </div><br>
+            </div>
+
+            <div class="col">
+            <h4 class="text-right"><strong>Visit Us</strong></h4>
+            <p class="text-right"><br>
+            <i class="fa fa-map-marker"></i>&nbsp;<em>Henley Beach</em><br><br>
+            email: Savoy@gmail.com<br>
+            phone: +00 (123) 456 7890</p>
+            </div>
+
+	</div>
+    </div>
+</section>
+<!--end of main page map section-->
+
+
+<?php
+require "footer.php";
+?>
